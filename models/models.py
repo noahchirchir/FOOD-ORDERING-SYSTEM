cdc0b5adbbcfc5caf23bd5239ef8f8d8bae680e9
ElvisKiongo <elvis.kiongo@student.moringaschool.com>
2024-06-12 09:34:37 +0300

    Slight changes to models.py

--- models/models.py
+++ models/models.py
@@ -14,6 +14,7 @@ class Customer(Base):
     email = Column(String, nullable=False)
 
     orders = relationship('Order', back_populates='customer')
+    reviews = relationship('Review', back_populates='customer')
 
     @classmethod
     def create(cls, name, email):
@@ -26,67 +27,59 @@ class Customer(Base):
     def get_by_id(cls, customer_id):
         return session.query(cls).filter_by(id=customer_id).first()
 
-    # Other ORM method for Customer class
 
 class Menu(Base):
     __tablename__ = 'menu'
 
-    menu_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     item_name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
 
-    # ORM method for Menu class
 
 class Review(Base):
     __tablename__ = 'reviews'
 
-    review_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey('customers.id'))
-    menu_id = Column(Integer, ForeignKey('menu.menu_id'))
+    menu_id = Column(Integer, ForeignKey('menu.id'))
     rating = Column(Integer, nullable=False)
-    comment = Column(String)
-    review_date = Column(DateTime, default=datetime.now)
 
     customer = relationship('Customer', back_populates='reviews')
     menu = relationship('Menu')
 
-    # ORM method for Review class
 
 class Order(Base):
     __tablename__ = 'orders'
 
-    order_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     customer_id = Column(Integer, ForeignKey('customers.id'))
-    order_date = Column(DateTime, default=datetime.now)
 
     customer = relationship('Customer', back_populates='orders')
     order_items = relationship('OrderItem', back_populates='order')
 
     @classmethod
-    def create(cls, customer_id, order_date):
-        new_order = cls(customer_id=customer_id, order_date=order_date)
+    def create(cls, customer_id):
+        new_order = cls(customer_id=customer_id)
         session.add(new_order)
         session.commit()
         return new_order
 
     @classmethod
     def get_by_id(cls, order_id):
-        return session.query(cls).filter_by(order_id=order_id).first()
+        return session.query(cls).filter_by(id=order_id).first()
 
-    # Other ORM method for Order class
 
 class OrderItem(Base):
     __tablename__ = 'order_items'
 
-    order_item_id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey('orders.order_id'))
-    menu_id = Column(Integer, ForeignKey('menu.menu_id'))
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    menu_id = Column(Integer, ForeignKey('menu.id'))
     quantity = Column(Integer, nullable=False)
 
     order = relationship('Order', back_populates='order_items')
     menu = relationship('Menu')
-
-    # ORM method for OrderItem class
 
 # Create the database engine and tables
 engine = create_engine('sqlite:///food_ordering.db')
